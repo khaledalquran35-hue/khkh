755c6721b3db412162a6eac8c79613b818c46735
lad <khaledalquran35@gmail.com>
2026-03-01 21:46:32 +0300

    new code

--- khkh/Program.cs
+++ khkh/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*using System.ComponentModel.Design;
+
+Console.WriteLine("please enter price ");
+double price =Convert.ToInt32( Console.ReadLine());
+Console.WriteLine(price);
+
+if (price >= 500){
+   price-=0.2;
+    Console.WriteLine($"final price:{price}");
+}
+else if (price >= 300 && price <= 499){
+    price -= 0.1;
+
+    Console.WriteLine($"final price:{price}");
+}
+ else if (price < 300){
+    Console.WriteLine($"final price:{price}");
+}
+else{
+    Console.WriteLine("not price");
+}
+
+
+
+
+*/
+
+
+
+
+int var_1 = 9;
+var var_2 = "lklk";
+var_1 = var_2 == "lklk" ? var_1++ : var_1--;
